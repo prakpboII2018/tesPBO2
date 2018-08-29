b69dbeb1949e82562e23e01837813f300937f1d1
admin <admin@19b>
2018-08-29 11:23:35 +0700

    versi 0.0

---
+++ src/AntrianKlinik/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AntrianKlinik;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Pasien pasien = new Pasien();
+        Dokter dokter = new Dokter();
+        //Set Pasien
+        pasien.setNama("Gregorius Bryan Osaldi");
+        pasien.setAlamat("Jalan Paingan 6, Maguwoharjo, Kab.Sleman, Daerah Istimewa Yogyakarta");
+        pasien.setTempatLahir("Sukaraya");
+        try {
+            pasien.setTanggalLahir(3);
+            pasien.setBulanLahir(05);
+            pasien.setTahunLahir(2000);
+            pasien.setNoRekamMedis("GREG");
+            dokter.setTanggalLahir(20);
+            dokter.setBulanLahir(07);
+            dokter.setTahunLahir(1980);
+        } catch (Exception ex) {
+            System.out.println(ex);;
+        }
+        //Set Dokter
+        dokter.setNama("KIWIL");
+        dokter.setNomorPegawai("1213245664");
+        dokter.setTempatLahir("Jogjakarta");
+        dokter.setAlamat("Maguwoharjo");
+
+        //Menampilkan Output
+        pasien.printInfo();
+        dokter.printInfo();
+
+    }
+
+}
